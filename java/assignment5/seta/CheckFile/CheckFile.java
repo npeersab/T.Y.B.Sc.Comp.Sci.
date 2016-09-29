fea6f8b6276c665ea11a7e1d115d62de0850069a
npeersab <npeersab77@gmail.com>
2016-09-29 20:25:24 +0530

    added checkfile

---
+++ java/assignment5/seta/CheckFile/CheckFile.java
@@ -0,0 +1,47 @@
+import java.io.File;
+import java.util.Scanner;
+import java.io.*;
+
+public class CheckFile {
+
+	public static void main(String args[]) throws IOException {
+
+		if(args.length == 0) {
+			System.out.println("Error : no input");
+			System.exit(0);
+		}
+
+		File file = new File(args[0]);
+		if(file == null) {
+			System.out.println("Error : file not found");
+		}
+
+		if(file.isDirectory()) {
+
+			String files[] = file.list();
+			System.out.println("Content of Directory :");
+			for(int i = 0; i < files.length; i++) {	
+					System.out.println(files[i]);
+			}
+			for(int i = 0; i < files.length; i++) {
+				File temp = new File(file.getPath() + "/" + files[i]);
+				if(temp.isFile())
+					if(files[i].indexOf(".txt") == files[i].length() - 4) {
+						System.out.print("Do you want to delete " + files[i] + " (y/n) ? : ");
+						BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+						char ch = (char) br.read();
+						if(ch == 'y' || ch == 'Y')
+							temp.delete();
+					}
+			}
+		}
+
+		else if(file.isFile()) {
+
+			System.out.println("File Information :");
+			System.out.println("Name :  " + file.getName());
+			System.out.println("Path : " + file.getAbsolutePath());
+			System.out.println("Size : " + file.length() + " bytes");
+		}
+	}
+}
---
+++ java/assignment5/seta/Phone/Phone.java
@@ -0,0 +1,11 @@
+public class Phone {
+
+	public static void main(String args[]) {
+
+		File file = new File("phone.txt");
+
+		if(file == null) {
+
+		}
+	}
+}
