690df281bce871acfdf8fc8a3f60749283c2283d
Noor Peersab <npeersab77@gmail.com>
2016-10-16 21:38:41 +0530

    formatted code

--- Java/assignment2/setb/Number/Series/Prime.java
+++ Java/assignment2/setb/Number/Series/Prime.java
@@ -5,12 +5,12 @@ public class Prime {
 	// displau n prime numbers
 	public static void displayPrime(int n) {
 
-		for(int i = 0, j = 2; i < n; j++) {
+		for (int i = 0, j = 2; i < n; j++) {
 			int k;
-			for(k = 2; k < j; k++)
-				if(j % k == 0)
+			for (k = 2; k < j; k++)
+				if (j % k == 0)
 					break;
-			if(k == j) {
+			if (k == j) {
 				System.out.print(j + " ");
 				i++;
 			}
