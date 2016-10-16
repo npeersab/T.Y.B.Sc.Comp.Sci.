cb7c4723e1f3f3a96b2c743a28265dfd0e561241
Noor Peersab <npeersab77@gmail.com>
2016-10-16 21:38:54 +0530

    formatted code

--- Java/assignment2/setb/Number/Series/Square.java
+++ Java/assignment2/setb/Number/Series/Square.java
@@ -5,7 +5,7 @@ public class Square {
 	// display n terms of series of square e.g. 1, 4, 9, 16 ...
 	public static void displaySquare(int n) {
 
-		for(int i = 1; i <= n; i++)
+		for (int i = 1; i <= n; i++)
 			System.out.print((i * i) + " ");
 	}
 }
